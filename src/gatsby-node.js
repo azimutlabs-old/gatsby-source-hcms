d59ccfb055e545eede04ac60533353c100d43db2
Daniyar <31.10.93@bk.ru>
2018-12-15 21:44:17 +0600

    Rearranged some posiitons of lines of code for better looks. Added additional console log in order to check changes

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,15 +1,35 @@
 const fetch = require('node-fetch')
 const queryString = require('query-string')
 
+/**
+ *  This override will try to fetch data as tree
+ *
+ *  Mussabekov Daniyar
+ */
+
 exports.sourceNodes = async (
   { actions, createNodeId, createContentDigest },
-  configOptions
+  configOptions,
 ) => {
   const { createNode } = actions
-
+  
+  console.log('!!!STARTED HCMS!!!!')
+  
   // Gatsby adds a configOption that's not needed for this plugin, delete it
   delete configOptions.plugins
 
+  // Setting root url to API
+  const apiUrl = `${configOptions.apiURL}`
+  // Setting Authorization token for API
+  let headers = new fetch.Headers()
+  headers.append('Authorization', `Bearer ${configOptions.key}`)
+
+
+  /**
+   * This method will try to create data for GraphQL nodes
+   * @param entity JSON object data fetched from API
+   * @returns {any} JSON object if node
+   */
   const processEntity = entity => {
     const nodeId = createNodeId(`hcms-${entity.entity}-${entity.id}`)
     const nodeContent = JSON.stringify(entity)
@@ -31,20 +51,23 @@ exports.sourceNodes = async (
     return nodeData
   }
 
-  const apiUrl = `${configOptions.apiURL}`
 
-  let headers = new fetch.Headers()
-  headers.append('Authorization', `Bearer ${configOptions.key}`)
-
+  // These are content types which were created aside from
+  // dynamically added content types
   let contentTypes = [
     {
       description: 'projects',
-      url: 'projects',
+      url: 'projects/tree',
       slug: 'project',
     },
     {
+      description: 'categories',
+      url: 'categories/tree',
+      slug: 'categories',
+    },
+    {
       description: 'pages',
-      url: `projects/${process.env.PROJECT_SLUG}/pages`,
+      url: `projects/${process.env.PROJECT_SLUG}/pages/tree`,
       slug: 'page',
     },
     {
